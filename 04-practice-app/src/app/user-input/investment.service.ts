16b6014e1e36a4ea725f81a71d57be0132551025
giorgi-tsereteli <g.tsereteli@elsevier.com>
2025-02-15 22:43:35 -0800

    Add test code for the service

---
+++ 04-practice-app/src/app/user-input/investment.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InvestmentResultsService {
+  onCalculateInvestmentResults(
+    initialInvestment: any,
+    annualInvestment: any,
+    expectedReturn: any,
+    duration: any
+  ) {
+    initialInvestment = Number(initialInvestment);
+    annualInvestment = Number(annualInvestment);
+    expectedReturn = Number(expectedReturn);
+    duration = Number(duration);
+
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    return annualData;
+  }
+}
+
