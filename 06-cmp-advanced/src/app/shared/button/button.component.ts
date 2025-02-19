aaebd552f192e11b364512035b583eb1cc336d3f
giorgi-tsereteli <g.tsereteli@elsevier.com>
2025-02-18 22:42:22 -0800

    Add host selector instead of button selector in css to target component

--- 06-cmp-advanced/src/app/shared/button/button.component.ts
+++ 06-cmp-advanced/src/app/shared/button/button.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   // You tell angular to use this component when a button element has the appButton attribute.
   // You render the component templates depending on the selectors like class or attribute. Check official docs.
   // Part 104 from the 6th section of the course
-  selector: 'button[appButton]',
+  selector: 'button[appButton], a[appButton]',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
