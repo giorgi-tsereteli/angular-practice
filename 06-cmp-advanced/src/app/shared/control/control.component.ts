fffcd385cabd25d7af8c601edcf7422a9c777373
giorgi-tsereteli <g.tsereteli@elsevier.com>
2025-02-19 22:53:10 -0800

    Update control component

--- 06-cmp-advanced/src/app/shared/control/control.component.ts
+++ 06-cmp-advanced/src/app/shared/control/control.component.ts
@@ -6,7 +6,12 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    // Prior, u added class names in the template each time when u used the component
+    // Now, u can add class names here and they will be added to the host element
+    class: 'control',
+  },
 })
 export class ControlComponent {
   @Input({ required: true }) label!: string;
