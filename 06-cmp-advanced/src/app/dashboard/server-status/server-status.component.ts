df9e10943b22d977c867601e951f2194932efdb7
giorgi-tsereteli <g.tsereteli@elsevier.com>
2025-02-22 19:41:53 -0800

    Implement interface and create string literals

--- 06-cmp-advanced/src/app/dashboard/server-status/server-status.component.ts
+++ 06-cmp-advanced/src/app/dashboard/server-status/server-status.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { DashboardItemComponent } from "../dashboard-item/dashboard-item.component";
+import { Component, OnInit } from '@angular/core';
+import { DashboardItemComponent } from '../dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-server-status',
@@ -8,6 +8,41 @@ import { DashboardItemComponent } from "../dashboard-item/dashboard-item.compone
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+
+// Implement OnInit interface to use ngOnInit() method
+// Not required but a good practice to implement and then use the method
+export class ServerStatusComponent implements OnInit {
+  // String literal helps with typos - only 'online', 'offline' or 'unknown' are allowed
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+
+  constructor() {
+    // Simulate server status change
+    // setInterval(() => {
+    //   const rnd = Math.random(); // Random number between 0 and 1
+    //   if (rnd < 0.5) {
+    //     this.currentStatus = 'online';
+    //   } else if (rnd < 0.9) {
+    //     this.currentStatus = 'offline';
+    //   } else {
+    //     this.currentStatus = 'unknown';
+    //   }
+    // }, 1000);
+  }
+
+  // This method is called when the component is initialized
+  // It's better to use ngOnInit() instead of the constructor for such actions
+  ngOnInit() {
+    console.log('ServerStatusComponent initialized');
+    // Simulate server status change
+    setInterval(() => {
+      const rnd = Math.random(); // Random number between 0 and 1
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 1000);
+  }
 }
