b1ac0e8160c6a17e8f2c52f361531a1d26fe9c88
giorgi-tsereteli <g.tsereteli@elsevier.com>
2025-02-22 21:42:00 -0800

    Add practice code for ngOnDestroy

--- 06-cmp-advanced/src/app/dashboard/server-status/server-status.component.ts
+++ 06-cmp-advanced/src/app/dashboard/server-status/server-status.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardItemComponent } from '../dashboard-item/dashboard-item.component';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-server-status',
   standalone: true,
-  imports: [DashboardItemComponent],
+  imports: [],
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
@@ -12,8 +13,12 @@ import { DashboardItemComponent } from '../dashboard-item/dashboard-item.compone
 // Implement OnInit interface to use ngOnInit() method
 // Not required but a good practice to implement and then use the method
 export class ServerStatusComponent implements OnInit {
+  
   // String literal helps with typos - only 'online', 'offline' or 'unknown' are allowed
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+
+  // Creating interval variable to store setInterval() method
+  private interval?: ReturnType<typeof setInterval>;
 
   constructor() {
     // Simulate server status change
@@ -32,9 +37,9 @@ export class ServerStatusComponent implements OnInit {
   // This method is called when the component is initialized
   // It's better to use ngOnInit() instead of the constructor for such actions
   ngOnInit() {
-    console.log('ServerStatusComponent initialized');
+    console.log('ngOnInit initialized');
     // Simulate server status change
-    setInterval(() => {
+    this.interval = setInterval(() => {
       const rnd = Math.random(); // Random number between 0 and 1
       if (rnd < 0.5) {
         this.currentStatus = 'online';
@@ -44,5 +49,15 @@ export class ServerStatusComponent implements OnInit {
         this.currentStatus = 'unknown';
       }
     }, 1000);
+  }
+
+  // ngAfterViewInit() {
+  //   console.log('ngAfterViewInit initialized');
+  // }
+
+  // Just a practice of cleaning timouts after ng destroy
+  // Currently component is never destroyed so this won't work
+  ngOnDestroy() {
+    clearTimeout(this.interval);
   }
 }
