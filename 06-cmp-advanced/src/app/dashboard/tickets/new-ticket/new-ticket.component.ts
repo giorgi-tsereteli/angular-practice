905c7360c53793b3dac18978a35a0df0ea5a7e0d
giorgi-tsereteli <g.tsereteli@elsevier.com>
2025-02-24 10:54:37 -0800

    Add comments and form reset option

--- 06-cmp-advanced/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 06-cmp-advanced/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -12,10 +12,21 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTicketComponent {
 
-  onSubmit(title: string, description: string) {
+  // ViewChild decorator is used to access the elements in the template
+  // It makes these elements available to the component class
+  // You can also pass classnames and interact with the instanc ef that element. You can NOT pass css class like .form
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+
+  onSubmit(title: string, description: string, form: HTMLFormElement) {
     console.log(title);
     console.log(description);
+    console.log(form);
     
+    // Reset the form after submission using the #form template variable from the components template
+    // Question mark is needed since the value might not be available at the time of the call
+    // Since rest works on the form element, you need to access the nativeElement property of the form variable
+    // In previous example, you just passed template variable form into the function and didn't use ViewChild at all
+    this.form?.nativeElement.reset();
   }
 
 }
