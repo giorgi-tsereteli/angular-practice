c7f35d7f5a0edf80619371ae2451a0bd6591245c
giorgi-tsereteli <g.tsereteli@elsevier.com>
2025-02-25 16:25:34 -0800

    Add functionality to mark tickets completed

--- 06-cmp-advanced/src/app/dashboard/tickets/ticket/ticket.component.ts
+++ 06-cmp-advanced/src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output, Output, signal } from '@angular/core';
 import { Ticket } from '../ticket.model';
 
 @Component({
@@ -10,4 +10,16 @@ import { Ticket } from '../ticket.model';
 })
 export class TicketComponent {
   data = input.required<Ticket>();
+  close = output();
+  detailsVisible = signal(false);
+
+  onToggleDetails(): void {
+    // Following allows to keep clicking button and toggling the details on and off
+    this.detailsVisible.set(!this.detailsVisible());
+  }
+
+  // Emitting nothing just for practice purpose. This help to captchure the event in tickets.component
+  onMarkAsCompleted() {
+    this.close.emit();
+  }
 }
--- 06-cmp-advanced/src/app/dashboard/tickets/tickets.component.ts
+++ 06-cmp-advanced/src/app/dashboard/tickets/tickets.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { NewTicketComponent } from './new-ticket/new-ticket.component';
 import { Ticket } from './ticket.model';
-import { TicketComponent } from "./ticket/ticket.component";
+import { TicketComponent } from './ticket/ticket.component';
+import { tick } from '@angular/core/testing';
 
 @Component({
   selector: 'app-tickets',
@@ -27,7 +28,17 @@ export class TicketsComponent {
       title: ticketData.title,
       request: ticketData.ticketText,
       status: 'open',
-    }
+    };
     this.tickets.push(ticket);
+  }
+
+  onCloseTicket(id: string) {
+    // console.log('Closing ticket with id:', id);
+    this.tickets = this.tickets.map((ticket) => {
+      if (ticket.id === id) {
+        return { ...ticket, status: 'closed' };
+      }
+      return ticket;
+    });
   }
 }
